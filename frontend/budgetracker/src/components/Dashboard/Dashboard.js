e5adbaa9ccc7a21bab41ada3eec78c9fa2607e75
Fidele Sorgho <srgfid2@gmail.com>
2024-08-17 00:44:12 +0100

    Additions to income and expenses, chart created

--- frontend/budgetracker/src/components/Dashboard/Dashboard.js
+++ frontend/budgetracker/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../../syles/Layouts";
+import Chart from "../Chart/Chart";
+import { useGlobalContext } from "../../context/global";
 
 function Dashboard() {
+    const {totalExpenses, totalIncome} = useGlobalContext()
     return (
         <DashboardStyled>
             Dashboard
             <InnerLayout>
+                <h1> All Transactions History </h1>
+                <div className="stats-con">
+                    <div className="chart-con">
+                        <Chart />
+                        <div className="amount-con">
+                            <div className="income">
+                                <h2>Total Income</h2>
+                                <p>£ {totalIncome()}</p>
+                            </div>
+                            <div className="expense">
+                                <h2>Total Expenses</h2>
+                                <p>£ {totalExpenses()}</p>
+                            </div>
+                            <div className="balance">
+                                <h2>Balance</h2>
+                                <p>£ {totalIncome() - totalExpenses()}</p>
+                            </div>
+                        </div>
+                    </div>
 
+                </div>
             </InnerLayout>
 
         </DashboardStyled>
